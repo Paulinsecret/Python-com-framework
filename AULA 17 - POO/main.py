0f65801376ca0c6fdf6bfeab313d1b2c55c7697c
Paulinsecret <paulorpcarvalho2507@gmail.com>
2026-05-23 11:41:59 -0300

    Add files via upload

---
+++ AULA 17 - POO/main.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+
+
+
+class Portifolio:
+    def __init__(self):
+        nome = st.title("Paulo Roberto Parnaia de Carvalho")
+        link = st.write('https')
+        whats = st.write('(11) 96031-8886')
+        email = st.success('Rua elizabete, 299, Guaruhos - SP')
+        img = st.image('img.jpg')
+        audio = st.audio('som.mp3')
+
+
+
+nome = Portifolio()                         
