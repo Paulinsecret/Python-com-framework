3a3776d14bf5b000cdb500433654e0baa2edb272
Paulinsecret <paulorpcarvalho2507@gmail.com>
2026-05-30 12:30:20 -0300

    Add files via upload

---
+++ Aula 19 - Arquitetura de software e padrões de design/model.py
@@ -0,0 +1,22 @@
+# banco de dados -  modelagem  
+
+import sqlite3
+
+def salvar_no_banco(texto):
+    conn = sqlite3.connect('Aula 19 - Arquitetura de software e padrões de design/banco.db')
+    c =  conn.cursor()
+    c.execute("CREATE TABLE IF NOT EXISTS notas (texto TEXT)")
+    c.execute("INSERT INTO notas VALUES(?)",(texto,))
+    conn.commit()
+    conn.close()
+
+def ler_do_banco():
+    conn = sqlite3.connect('Aula 19 - Arquitetura de software e padrões de design/banco.db')
+    c =  conn.cursor()
+    c.execute('CREATE TABLE IF NOT EXISTS notas(texto TEXT)')
+    c.execute("SELECT * FROM notas")
+   
+    dados  =  c.fetchall()
+    conn.close()
+    return dados
+
